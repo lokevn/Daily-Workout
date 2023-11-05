fc438a40ba332473b5c38ded9c03bddaed8ca004
loke pc <baoviet2105@gmail.com>
2023-11-05 13:40:33 +0700

    exercise progress

--- app/src/main/java/com/example/dailyworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/dailyworkout/ExerciseActivity.kt
@@ -2,10 +2,15 @@ package com.example.dailyworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.dailyworkout.databinding.ActivityExerciseBinding
-
 class ExerciseActivity : AppCompatActivity() {
     private var exerciseBinding:ActivityExerciseBinding? = null
+    private var countDownTimer: CountDownTimer? = null
+    private var timerDuration: Long = 10000
+    private var pauseOffset: Long = 0
+    private val countInterval: Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         exerciseBinding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -19,11 +24,44 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        resetProgressBar()
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
         exerciseBinding = null
+        if (countDownTimer != null) {
+            countDownTimer?.cancel()
+            pauseOffset = 0
+        }
+    }
+
+    private fun setProgressBar() {
+        countDownTimer = object: CountDownTimer(timerDuration, countInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                pauseOffset = timerDuration - millisUntilFinished
+                exerciseBinding?.exersisePB?.progress = (millisUntilFinished/1000+1).toInt()
+                exerciseBinding?.timerTV?.text = (millisUntilFinished/1000+1).toString()
+            }
+
+            override fun onFinish() {
+                pauseOffset += countInterval
+                exerciseBinding?.exersisePB?.progress = (10-pauseOffset/1000).toInt()
+                exerciseBinding?.timerTV?.text = (10-pauseOffset/1000).toString()
+                Toast.makeText(this@ExerciseActivity,
+                    "Now exercise will start",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    private fun resetProgressBar() {
+        if (countDownTimer != null) {
+            countDownTimer?.cancel()
+            pauseOffset = 0
+        }
+        setProgressBar()
     }
 }
