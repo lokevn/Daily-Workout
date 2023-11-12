d5ce68abc2bad34d428e3960900e68e42ee2c2d4
loke pc <baoviet2105@gmail.com>
2023-11-12 17:19:00 +0700

    tts and sound play

---
+++ app/src/main/java/com/example/dailyworkout/ExerciseStatusAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.dailyworkout
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dailyworkout.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items:ArrayList<ExerciseModel>):
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+        class ViewHolder(binding:ItemExerciseStatusBinding):
+            RecyclerView.ViewHolder(binding.root) {
+                val tvItem = binding.nameTV
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemExerciseStatusBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model:ExerciseModel = items[position]
+        holder.tvItem.text = model.getId().toString()
+    }
+}
